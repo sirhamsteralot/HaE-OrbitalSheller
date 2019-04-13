a3ee0c1a905d954d5d8979160603fc7a508695a6
sirhamsteralot <sirhamsteralot@gmail.com>
2019-04-13 23:15:08 +0200

    Targeting

--- HaE OrbitalSheller/Program.cs
+++ HaE OrbitalSheller/Program.cs
@@ -20,20 +20,63 @@ namespace IngameScript
 {
     partial class Program : MyGridProgram
     {
+        Scheduler scheduler;
+        INISerializer cannonSettings;
 
+        Cannon cannon;
+        Targeter targeter;
 
         public Program()
         {
-
+            scheduler = new Scheduler();
+            scheduler.AddTask(Init());
+            Runtime.UpdateFrequency = UpdateFrequency.Update1;
         }
 
-        public void Save()
+        public IEnumerator<bool> Init()
         {
+            cannonSettings = new INISerializer("CannonSettings");
+            cannonSettings.AddValue("referenceName", x => x, "RCReference");
+            cannonSettings.AddValue("speedCap", x => double.Parse(x), "RCReference");
+            cannonSettings.AddValue("launchVelocity", x => double.Parse(x), "RCReference");
+
+
+            if (Me.CustomData == "")
+            {
+                string temp = Me.CustomData;
+                cannonSettings.FirstSerialization(ref temp);
+                Me.CustomData = temp;
+            }
+            else
+            {
+                cannonSettings.DeSerialize(Me.CustomData);
+            }
+            yield return true;
+            IMyShipController reference = GridTerminalSystem.GetBlockWithName((string)cannonSettings.GetValue("referenceName")) as IMyShipController;
+
+            yield return true;
+            targeter = new Targeter
+                (
+                    (double)cannonSettings.GetValue("speedCap"),
+                    (double)cannonSettings.GetValue("launchVelocity"),
+                    reference
+                );
+            targeter.directionFoundCallback += TargetCalculatedCallback;
+
+            yield return true;
+
 
         }
+
 
         public void Main(string argument, UpdateType updateSource)
         {
+            targeter.TargetingLoop();
+        }
+
+        public void TargetCalculatedCallback(Vector3D position)
+        {
+
         }
     }
 }
---
+++ HaE OrbitalSheller/Targeter.cs
@@ -0,0 +1,61 @@
+﻿using Sandbox.Game.EntityComponents;
+using Sandbox.ModAPI.Ingame;
+using Sandbox.ModAPI.Interfaces;
+using SpaceEngineers.Game.ModAPI.Ingame;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System;
+using VRage;
+using VRage.Collections;
+using VRage.Game.Components;
+using VRage.Game.ModAPI.Ingame;
+using VRage.Game.ModAPI.Ingame.Utilities;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game;
+using VRageMath;
+
+namespace IngameScript
+{
+    partial class Program
+    {
+        public class Targeter
+        {
+            public double speedCap;
+            public double launchVelocity;
+            public IMyShipController reference;
+
+            public Action<Vector3D> directionFoundCallback;
+
+            private Simulated_Targeting simTargeting;
+            private bool fireCallbackOnce = false;
+
+            public Targeter(double speedCap, double launchVelocity, IMyShipController reference)
+            {
+                this.speedCap = speedCap;
+                this.launchVelocity = launchVelocity;
+                this.reference = reference;
+            }
+
+            public void TargetPosition(Vector3D position, MyDetectedEntityInfo planet)
+            {
+                Vector3D startPos = reference.GetPosition();
+                Vector3D direction = Vector3D.Normalize(position - startPos);
+
+                simTargeting = new Simulated_Targeting(reference, position, startPos, direction, 0, planet, 9.81, launchVelocity, speedCap);
+
+                fireCallbackOnce = false;
+            }
+
+            public void TargetingLoop()
+            {
+                if (simTargeting?.Calculate() ?? false && !fireCallbackOnce)
+                {
+                    directionFoundCallback?.Invoke(simTargeting.GetCurrentFiringDirection());
+                    fireCallbackOnce = true;
+                }
+            }
+        }
+    }
+}
